e0999d2a543181be6a2534a3c1d467d54c425c9a
holder <103839360+under-hodler@users.noreply.github.com>
2022-06-09 03:05:08 +0900

    Create CelebPhotoBooksSeason1.sol

---
+++ contracts/cpb-nft/CelebPhotoBooksSeason1.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "../openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "../openzeppelin-contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../openzeppelin-contracts/security/Pausable.sol";
+import "../openzeppelin-contracts/access/Ownable.sol";
+import "../openzeppelin-contracts/access/AccessControl.sol";
+import "../openzeppelin-contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract CelebPhotoBooksSeason1 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable, Ownable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string private _baseTokenURI;
+
+    constructor(string memory baseTokenURI) ERC721("CelebPhotoBooksSeason1", "CPB1") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _baseTokenURI = baseTokenURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string calldata newBaseTokenURI) public onlyOwner {
+        _baseTokenURI = newBaseTokenURI;
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _baseURI();
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function safeMint(address to, uint256 tokenId)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _safeMint(to, tokenId);
+    }
+
+    function safeBatchMint(address [] calldata toList, uint256 [] calldata tokenIdList) public {
+        require(toList.length == tokenIdList.length, "Array length is different");
+        uint256 i = 0;
+
+        for (i; i < toList.length; i ++) {
+            safeMint(toList[i], tokenIdList[i]);
+        }
+    }
+    
+    function safeBatchTransfer(address [] calldata toList, uint256 [] calldata tokenIdList) public {
+        require(toList.length == tokenIdList.length, "Array length is different");
+        uint256 i = 0;
+
+        for (i; i < toList.length; i ++) {
+            super.transferFrom(msg.sender, toList[i], tokenIdList[i]);
+        }
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
