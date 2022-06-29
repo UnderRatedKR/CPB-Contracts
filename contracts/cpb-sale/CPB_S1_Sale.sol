166dcc094d160b958fd82504cb1a983445a2a095
hodler <103839360+under-hodler@users.noreply.github.com>
2022-06-29 23:49:36 +0900

    Update CPB_S1_Sale.sol

--- contracts/cpb-sale/CPB_S1_Sale.sol
+++ contracts/cpb-sale/CPB_S1_Sale.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.9;
 
 import "../openzeppelin-contracts/access/Ownable.sol";
+import "../cpb-nft/CPB_S1.sol";
 
 contract CPB_S1_Sale is Ownable{
 
-    event setSaleStatus(uint256 status);
+    event SetSaleStatus(uint256 status);
+    event SetVault(address vault);
 
     enum SaleState {
         NOT_STARTED,
@@ -14,40 +16,49 @@ contract CPB_S1_Sale is Ownable{
         COMPLETED
     }
 
+    CPB_S1 public cpb;
     SaleState public saleState;
     uint256 public salePrice; //wei or peb
+    address payable public vault;
+    uint256 public saleLimit;
     
-    constructor (uint256 _salePrice) {
+    constructor (uint256 _salePrice, CPB_S1 _cpb, uint256 _saleLimit) {
         salePrice = _salePrice;
         saleState = SaleState.NOT_STARTED;
+        saleLimit = _saleLimit;
+        cpb = _cpb;
+    }
+
+    function setCpb(CPB_S1 _cpb) public onlyOwner {
+        cpb = _cpb;
+    }
+
+    function setVault(address payable _vault) public onlyOwner {
+        vault = _vault;
+        emit SetVault(_vault);
     }
 
     function setSalePrice(uint256 _salePrice) public onlyOwner {
         salePrice = _salePrice;
     }
 
-    function setSaleNotStarted(uint256 _salePrice) public onlyOwner {
+    function setSaleNotStarted() public onlyOwner {
         saleState = SaleState.NOT_STARTED;
+        emit SetSaleStatus(0);
     }
 
-    function setSaleInProgress(uint256 _salePrice) public onlyOwner {
+    function setSaleInProgress() public onlyOwner {
         saleState = SaleState.IN_PROGRESS;
+        emit SetSaleStatus(1);
     }
 
-    function setSalePaused(uint256 _salePrice) public onlyOwner {
+    function setSalePaused() public onlyOwner {
         saleState = SaleState.PAUSED;
+        emit SetSaleStatus(2);
     }
 
-    function setSaleCompleted(uint256 _salePrice) public onlyOwner {
+    function setSaleCompleted() public onlyOwner {
         saleState = SaleState.COMPLETED;
+        emit SetSaleStatus(3);
     }
-
-
-/*
-
-    function setSaleToken(address _token) public onlyOwner {
-        SaleToken = _token;
-    }*/
-
 }
-    
