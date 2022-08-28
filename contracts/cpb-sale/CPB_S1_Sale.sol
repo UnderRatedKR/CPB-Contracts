35a745d46ad9bd81261786523e50010c8b7dee1e
hodler <103839360+under-hodler@users.noreply.github.com>
2022-08-29 02:14:40 +0900

    whitelist

--- contracts/cpb-sale/CPB_S1_Sale.sol
+++ contracts/cpb-sale/CPB_S1_Sale.sol
@@ -7,7 +7,9 @@ import "../openzeppelin-contracts/token/ERC721/ERC721.sol";
 contract CPB_S1_Sale is Ownable{
 
     event SetSaleStatus(uint256 status);
+    event SetPrice(uint256 price);
     event SetVault(address vault);
+    event SetWhitelist(address wallet);
 
     enum SaleState {
         NOT_STARTED,
@@ -18,10 +20,12 @@ contract CPB_S1_Sale is Ownable{
 
     ERC721 public nft;
     SaleState public saleState;
-    uint256 public salePrice; //wei or pebt
+    uint256 public salePrice; //wei or peb
     address payable public vault;
     uint256 public saleLimit;
-    
+    mapping(address => bool) public whitelist;
+
+    //세일 계약 생성 시 초기 세일가, 판매할 nft, 최대 판매랑, 자금 전송 지갑 설정 필요
     constructor (uint256 _salePrice, ERC721 _nft, uint256 _saleLimit, address _vault) {
         require(_vault != address(0), "vault cannot be assigned with a zero address.");
         
@@ -29,6 +33,10 @@ contract CPB_S1_Sale is Ownable{
         saleState = SaleState.NOT_STARTED;
         saleLimit = _saleLimit;
         nft = _nft;
+    }
+
+    function mintKlay(uint256 unit) public payable {
+        nft.safeMint(msg.sender, nft.)
     }
 
     function setSaleNft(ERC721 _nft) public onlyOwner {
@@ -41,7 +49,9 @@ contract CPB_S1_Sale is Ownable{
     }
 
     function setSalePrice(uint256 _salePrice) public onlyOwner {
+        
         salePrice = _salePrice;
+        emit SetPrice(_salePrice);
     }
 
     function setSaleNotStarted() public onlyOwner {
@@ -62,5 +72,16 @@ contract CPB_S1_Sale is Ownable{
     function setSaleCompleted() public onlyOwner {
         saleState = SaleState.COMPLETED;
         emit SetSaleStatus(3);
+    }
+
+    function setWhitelist(address[] calldata _wallet) public onlyOwner {
+        for (uint256 i = 0; i < _wallet.length; i++) {
+            whitelist[_wallet[i]] = true;
+        }
+        emit SetWhitelist(_wallet);
+    }
+
+    function isWhitelist(address _wallet) public view returns (bool) {
+        return whitelists[_wallet];
     }
 }
